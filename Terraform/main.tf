a5f62a10f26e3233509b244e046066c35de5872a
Mariem Moula <moula.meriame@gmail.com>
2024-11-06 20:38:14 +0100

    Adding the test classes

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -99,28 +99,3 @@ resource "aws_eks_node_group" "my_node_group" {
     aws_eks_cluster.my_cluster
   ]
 }
-
-# Load balancer
-resource "aws_elb" "app_load_balancer" {
-  name               = "app-load-balancer"
-  availability_zones = ["us-east-1a"]
-
-  listener {
-    instance_port     = 8080
-    instance_protocol = "HTTP"
-    lb_port          = 80
-    lb_protocol      = "HTTP"
-  }
-
-  health_check {
-    target              = "HTTP:8080/"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold  = 2
-    unhealthy_threshold = 2
-  }
-
-  tags = {
-    Name = "app-load-balancer"
-  }
-}
